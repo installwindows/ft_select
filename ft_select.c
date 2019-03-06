ca94f8ece00bd2d7ff2f22a1c6607b43dc80304b
Vincent ARNAUD <varnaud@student.42.us.org>
2019-03-06 07:56:07 +0100

    dsa

--- ft_select.c
+++ ft_select.c
@@ -6,13 +6,18 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 21:13:33 by varnaud           #+#    #+#             */
-/*   Updated: 2017/04/28 21:48:51 by varnaud          ###   ########.fr       */
+/*   Updated: 2019/03/06 07:53:14 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-int		main(void)
+int		ft_putcap(int c)
+{
+	return ((int)write(1, &c, 1));
+}
+
+void	init_termcap(void)
 {
 	char	term_buffer[2048];
 	char	*term_type;
@@ -35,4 +40,12 @@ int		main(void)
 		ft_fprintf(2, "Terminal type `%s` is not defined.\n", term_type);
 		exit(1);
 	}
+}
+
+int		main(void)
+{
+	init_termcap();
+	//tputs(tgetstr("cl", NULL), 11, putchar);
+	tputs(tgoto(tgetstr("cm", NULL), 5, 5), 1, ft_putcap);
+	ft_printf("%dx%d\n", tgetnum("co"), tgetnum("li"));
 }
