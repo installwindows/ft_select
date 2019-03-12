b306dff59dd8c85c1e5811734986da471641fe8b
Vincent ARNAUD <varnaud@student.42.us.org>
2019-03-12 17:03:52 +0100

    memem

--- test.c
+++ test.c
@@ -5,8 +5,38 @@
 #include <string.h>
 #include <termcap.h>
 
+#define SKEY_UP "\e[A"
+#define SKEY_DOWN "\e[B"
+#define SKEY_RIGHT "\e[C"
+#define SKEY_LEFT "\e[D"
+
+#define KEY_UP (1 << 0)
+#define KEY_DOWN (1 << 1)
+#define KEY_RIGHT (1 << 2)
+#define KEY_LEFT (1 << 3)
+
+int		check_key(const char *key, int code, char *buffer)
+{
+	int		r = 0;
+
+	if (code & KEY_UP && !(code &= ~KEY_UP))
+		if (!strcmp(key, SKEY_UP) || !strcmp(key, tgetstr("ku", &buffer)))
+			r = 1;
+	if (code & KEY_DOWN && !(code &= ~KEY_DOWN))
+		if (!strcmp(key, SKEY_DOWN) || !strcmp(key, tgetstr("kd", &buffer)))
+			r = 1;
+	if (code & KEY_RIGHT && !(code &= ~KEY_RIGHT))
+		if (!strcmp(key, SKEY_RIGHT) || !strcmp(key, tgetstr("kr", &buffer)))
+			r = 1;
+	if (code & KEY_LEFT && !(code &= ~KEY_LEFT))
+		if (!strcmp(key, SKEY_LEFT) || !strcmp(key, tgetstr("kl", &buffer)))
+			r = 1;
+	return r;//(code ? (r && check_key(key, code, buffer)) : r);
+}
+
 int		main(void) {
 	char	c;
+	char	key[4];
 	char	*buffer;
 	buffer = malloc(2048);
 	static struct termios oldtio, newtio;
@@ -15,11 +45,12 @@ int		main(void) {
 	printf("%s", getenv("TERM"));
 	tcgetattr(0, &oldtio);
 	newtio = oldtio;
-	/*
 	newtio.c_lflag &= ~ICANON;
 	newtio.c_lflag &= ~ECHO;
 	newtio.c_cc[VMIN] = 1;
 	newtio.c_cc[VTIME] = 0;
+	newtio.c_oflag &= ~OPOST;
+	/*
 	newtio.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
                            | INLCR | IGNCR | ICRNL | IXON);
 	newtio.c_oflag &= ~OPOST;
@@ -32,28 +63,30 @@ int		main(void) {
 	printf("Give text:\n");
 	fflush(stdout);
 	while (1) {
-		read(0, &c, 1);
+		read(0, &key[0], 1);
 
-		if (c == 27) {
-			char key[4];
+		if (key[0] == 27) {
 			key[0] = 27;
-			read(0, &c, 1);
-			key[1] = c;
-			read(0, &c, 1);
-			key[2] = c;
+			read(0, &key[1], 1);
+			read(0, &key[2], 1);
 			key[3] = 0;
-			char * k_up = tgetstr("ku", &buffer);
-
-			if (strcmp(key, k_up) == 0)
+			if (check_key(key, KEY_UP, buffer))
 				printf("UP\n");
-
-			if (strcmp(key, "\e[B") == 0)
+			else if (check_key(key, KEY_DOWN, buffer))
 				printf("DOWN\n");
+			else if (check_key(key, KEY_RIGHT, buffer))
+				printf("RIGHT\n");
+			else if (check_key(key, KEY_LEFT, buffer))
+				printf("LEFT\n");
+			else
+				printf("\\e%c%c\n", key[1], key[2]);
 			continue;
 		}
-		if (c == 'q') {
+		else if (key[0] == 'q') {
 			break;
 		}
+		else
+			printf(" %c ", key[0]);
 	}
 	printf("\n");
 	tcsetattr(0, TCSANOW, &oldtio);
