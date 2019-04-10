b2dd9f69284ee4937bef13320d32131776467e19
Vincent ARNAUD <varnaud@student.42.us.org>
2019-04-10 15:20:53 +0200

    gewurstraminer

--- readme.md
+++ readme.md
@@ -50,16 +50,43 @@ S'il n'y a aucune erreur, les fonctions **tgetflag**, **tgetnum**, **tgetstr**, 
 Canonique ou non?
 -----------------
 
+Un terminal peut être deux modes: canonique et non-canonique. Dans le premier mode, l'input de l'utilisateur est retourné par `read()` ligne par ligne, soit après avoir appuyé sur la touche <kbd>Enter</kbd>. Dans le second mode, chaque touche est directement retourné par `read()`.
+Le terminal a des attributs affectant son interaction avec l'utilisateur. Il peut par exemple output chaque input de l'utilisateur ou automatiquement revenir au début de la ligne suivante après output un `\n`.
+
+Pour faire des changements, on peut utiliser `ioctl()`, ou bien `tcsetattr()`. Pour ce qui est du second cas, j'utilise deux structures, une pour l'ancienne configuration `struct termios oldtio;` et une pour la nouvelle `struct termios newtio;`. `newtio` sera dès lors prête à être modifié et appliqué au terminal et `oldtio` devra être réappliqué lorsqu'on quitte le programme, car autrement, la nouvelle configuration sera persistante dans le terminal ayant lancé notre programme. `oldtio` est initializée par `tcgetattr(0, &oldtio);` et `newtio` par `newtio = oldtio;`. Une fois les modifications faites, on utilise `tcsetattr(0, TCANOW, &newtio);` pour setter la nouvelle configuration. Et rendu à la fin du programme: `tcsetattr(0, TCANOW, &oldtio);`.
+
+
+Flag | Description
+--- | ---
+**c\_iflag**
+BRKINT | Allow BREAK condition, which is a bunch of zeroes to do seemingly legacy stuff?
+ECHO | Echo input
+ECHOCTL | Echo control characters other than tab, newline, START and STOP
+ECHOE | ERASE is performed visually as \b space \b
+ECHOK
+ECHOKE | asjsdlkf 
 
 
 Termcaps
 --------
+
 Liste de mots magiques utiles:
 
-Mot magique | Description | Fonction
+Mot magique | Description | Exemple
 --- | --- | ---
-**co** | Screen width in lines | `int width = tgetnum("co")`
-**li** | Screen height in lines | `int height = tgetnum("li")`
+-- | **Screen information** | --
+`co` | Screen width in lines | `int width = tgetnum("co")`
+`li` | Screen height in lines | `int height = tgetnum("li")`
+ | **Cursor motion** |
+`cm` | Position cursor at line _l_, column _c_ | `tputs(tgoto(tgetstr("cm", NULL), c, l), 0, putchar)`
+`ho` | Move cursor to upper left corner (home) |
+`ll` | Move cursor to lower left corner |
+`cr` | Move cursor to begining of line |
+-- | **Clear parts of sreen** | --
+`cl` | Empty the screen of any characters and position the cursor at the upper left corner |
+`cd` | The cursor current line is anniliated and so is the lines bellow it |
+`ce` | Kills from the cursor to the end of line |
+`ec` | Remove `n` chars right of the cursor |
 
 
 
--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 #include <termios.h>
 #include <string.h>
 #include <termcap.h>
+#include "libft.h"
 
 #define SKEY_UP "\e[A"
 #define SKEY_DOWN "\e[B"
@@ -15,35 +16,36 @@
 #define KEY_RIGHT (1 << 2)
 #define KEY_LEFT (1 << 3)
 
-int		check_key(const char *key, int code, char *buffer)
+void	init_termcap(void)
 {
-	int		r = 0;
+	char	term_buffer[2048];
+	char	*term_type;
+	int		r;
 
-	if (code & KEY_UP && !(code &= ~KEY_UP))
-		if (!strcmp(key, SKEY_UP) || !strcmp(key, tgetstr("ku", &buffer)))
-			r = 1;
-	if (code & KEY_DOWN && !(code &= ~KEY_DOWN))
-		if (!strcmp(key, SKEY_DOWN) || !strcmp(key, tgetstr("kd", &buffer)))
-			r = 1;
-	if (code & KEY_RIGHT && !(code &= ~KEY_RIGHT))
-		if (!strcmp(key, SKEY_RIGHT) || !strcmp(key, tgetstr("kr", &buffer)))
-			r = 1;
-	if (code & KEY_LEFT && !(code &= ~KEY_LEFT))
-		if (!strcmp(key, SKEY_LEFT) || !strcmp(key, tgetstr("kl", &buffer)))
-			r = 1;
-	return r;//(code ? (r && check_key(key, code, buffer)) : r);
+	term_type = getenv("TERM");
+	if (term_type == NULL)
+	{
+		ft_fprintf(2, "Environment variable TERM not set.\n");
+		exit(1);
+	}
+	r = tgetent(term_buffer, term_type);
+	if (r < 0)
+	{
+		ft_fprintf(2, "Could not access the termcap database\n");
+		exit(1);
+	}
+	else if (r == 0)
+	{
+		ft_fprintf(2, "Terminal type `%s` is not defined.\n", term_type);
+		exit(1);
+	}
 }
 
+struct termios	initialize_terminal(void)
+{
+	struct termios	oldtio;
+	struct termios	newtio;
 
-int		main(void) {
-	char	c;
-	char	key[4];
-	char	*buffer;
-	buffer = malloc(2048);
-	static struct termios oldtio, newtio;
-
-	tgetent(buffer, getenv("TERM"));
-	printf("%s", getenv("TERM"));
 	tcgetattr(0, &oldtio);
 	newtio = oldtio;
 	newtio.c_lflag &= ~ICANON;
@@ -60,27 +62,60 @@ int		main(void) {
 	newtio.c_cflag |= CS8;
 	*/
 	tcsetattr(0, TCSANOW, &newtio);
+	return (oldtio);
+}
 
-	printf("Give text:\n");
-	fflush(stdout);
+int		ft_putcap(int c)
+{
+	return ((int)write(1, &c, 1));
+}
+
+int		check_key(const char *key, int code)
+{
+	int		r = 0;
+
+	if (code & KEY_UP && !(code &= ~KEY_UP))
+		if (!strcmp(key, SKEY_UP) || !strcmp(key, tgetstr("ku", NULL)))
+			r = 1;
+	if (code & KEY_DOWN && !(code &= ~KEY_DOWN))
+		if (!strcmp(key, SKEY_DOWN) || !strcmp(key, tgetstr("kd", NULL)))
+			r = 1;
+	if (code & KEY_RIGHT && !(code &= ~KEY_RIGHT))
+		if (!strcmp(key, SKEY_RIGHT) || !strcmp(key, tgetstr("kr", NULL)))
+			r = 1;
+	if (code & KEY_LEFT && !(code &= ~KEY_LEFT))
+		if (!strcmp(key, SKEY_LEFT) || !strcmp(key, tgetstr("kl", NULL)))
+			r = 1;
+	return r;//(code ? (r && check_key(key, code, buffer)) : r);
+}
+
+void	loop_input(void)
+{
+	char			c;
+	char			key[4];
+
 	while (1) {
 		read(0, &key[0], 1);
+		//tputs(tgoto(tgetstr("cm", NULL), 0, 0), 0, ft_putcap);
+		//tputs(tgetstr("cl", NULL), 0, ft_putcap);
+		tputs(tgetstr("cr", NULL), 0, ft_putcap);
+		tputs(tgetstr("ce", NULL), 0, ft_putcap);
 
 		if (key[0] == 27) {
 			key[0] = 27;
 			read(0, &key[1], 2);
 			//read(0, &key[2], 1);
 			key[3] = 0;
-			if (check_key(key, KEY_UP, buffer))
-				printf("UP\n");
-			else if (check_key(key, KEY_DOWN, buffer))
-				printf("DOWN\n");
-			else if (check_key(key, KEY_RIGHT, buffer))
-				printf("RIGHT\n");
-			else if (check_key(key, KEY_LEFT, buffer))
-				printf("LEFT\n");
+			if (check_key(key, KEY_UP))
+				ft_printf("UP");
+			else if (check_key(key, KEY_DOWN))
+				ft_printf("DOWN");
+			else if (check_key(key, KEY_RIGHT))
+				ft_printf("RIGHT");
+			else if (check_key(key, KEY_LEFT))
+				ft_printf("LEFT");
 			else
-				printf("\\e%c%c\n", key[1], key[2]);
+				ft_printf("\\e%c%c\n", key[1], key[2]);
 			continue;
 		}
 		else if (key[0] == 'q') {
@@ -89,8 +124,14 @@ int		main(void) {
 		else
 			printf(" %c ", key[0]);
 	}
-	printf("\n");
-	tcsetattr(0, TCSANOW, &oldtio);
+}
 
+int		main(void) {
+	struct termios	oldtio;
+
+	init_termcap();
+	oldtio = initialize_terminal();
+	loop_input();
+	tcsetattr(0, TCSANOW, &oldtio);
 	return 0;
 }
