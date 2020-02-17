614bbdd44372efb6f178d4498e36f5e5df6f697c
Vincent Arnaud <varnaud@live.ca>
2020-02-17 22:06:18 +0100

    add longest word

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 21:10:55 by varnaud           #+#    #+#             */
-/*   Updated: 2020/02/10 21:16:27 by varnaud          ###   ########.fr       */
+/*   Updated: 2020/02/17 21:16:25 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,18 @@
 int		ft_putcap(int c)
 {
 	return ((int)write(1, &c, 1));
+}
+
+int		get_longest_word(t_word *words)
+{
+	int		lgw;
+
+	lgw = 0;
+	while (words)
+	{
+		if (words->len > lgw)
+			lgw = words->len;
+		words = words->next;
+	}
+	return (lgw);
 }
